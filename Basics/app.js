917dadbd4e9d310d28d5719dd5d0fb13ec01fdac
Anushree M <anushreemsbhat@gmail.com>
2025-05-08 15:20:42 +0530

    Redirecting req & parsing req body

--- Basics/app.js
+++ Basics/app.js
@@ -2,9 +2,13 @@
 
 const http = require("http");
 const { serialize } = require("v8");
+const fs = require("fs");
+const { log } = require("console");
+const { parse } = require("path");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
@@ -13,6 +17,25 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    // acts like event listener: for every incoming data piece
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      //fs.writeFileSync("message.txt", "Dummy");
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
